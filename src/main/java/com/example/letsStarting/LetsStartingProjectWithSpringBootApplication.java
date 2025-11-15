b9f152ccc23382bb3d6239a04ee43fb315c5c3ba
abhishek goud <goud87041@gmail.com>
2025-11-15 15:19:50 +0530

    say Hello with Spring

---
+++ src/main/java/com/example/letsStarting/LetsStartingProjectWithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.letsStarting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetsStartingProjectWithSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetsStartingProjectWithSpringBootApplication.class, args);
+	}
+
+}
